9daf13316fa1baa2abb67d6c40190ba52568d1f2
Кірюша <mkiril087@gmail.com>
2025-05-18 20:21:02 +0300

    gh

---
+++ main.js
@@ -0,0 +1,86 @@
+const weekDay = prompt(`enter day of the week`);
+switch (weekDay) {
+  case `monday`:
+    console.log(`work day`);
+    break;
+  case `tuesday`:
+    console.log(`work day`);
+    break;
+  case `wednesday`:
+    console.log(`work day`);
+    break;
+  case `thursday`:
+    console.log(`work day`);
+    break;
+  case `friday`:
+    console.log(`work day`);
+    break;
+  case `saturday`:
+    console.log(`holiday`);
+    break;
+  case `sunday`:
+    console.log(`holiday`);
+    break;
+  default:
+    console.log(`u entered smth wrong`);
+}
+
+const month = prompt(`enter month`);
+switch (month) {
+  case `1`:
+    console.log(`winter`);
+    break;
+  case `2`:
+    console.log(`winter`);
+    break;
+  case `3`:
+    console.log(`spring`);
+    break;
+  case `4`:
+    console.log(`spring`);
+    break;
+  case `5`:
+    console.log(`spring`);
+    break;
+  case `6`:
+    console.log(`summer`);
+    break;
+  case `7`:
+    console.log(`summer`);
+    break;
+  case `8`:
+    console.log(`summer`);
+    break;
+  case `9`:
+    console.log(`autumn`);
+    break;
+  case `10`:
+    console.log(`autumn`);
+    break;
+  case `11`:
+    console.log(`autumn`);
+    break;
+  case `12`:
+    console.log(`winter`);
+    break;
+
+  default:
+    console.log(`u entered smth wrong`);
+}
+
+const color = prompt(`enter colour of traffic light`);
+switch (color) {
+  case `yellow`:
+    console.log(`wait`);
+    break;
+  case `red`:
+    console.log(`wait`);
+    break;
+  case `green`:
+    console.log(`wait`);
+    break;
+  default:
+    console.log(`u entered smth wrong`);
+}
+
+
